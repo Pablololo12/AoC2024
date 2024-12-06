1298b4be27a3dcfbe0ed4fef0606990ff2b0e482
Pablololo12 <pabloheralm@gmail.com>
2024-12-06 09:28:37 +0000

    Day2 part2 solution multi process

--- src/y2024/day05.rs
+++ src/y2024/day05.rs
@@ -1,21 +1,23 @@
 use std::cmp::Ordering;
 
 pub fn parse_pages(inp: &Vec<String>) -> (Vec<(i64, i64)>, Vec<Vec<i64>>) {
-    let mut out: Vec<Vec<i64>> = vec![];
-    let mut output: Vec<(i64, i64)> = vec![];
-    inp.iter().for_each(|f| {
-        if f.contains(",") {
-            out.push(f.split(",").map(|jj| jj.parse::<i64>().unwrap()).collect());
-        }
-        if f.contains("|") {
-            let mut kk = f.split("|");
-            output.push((
+    let pages: Vec<Vec<i64>> = inp
+        .iter()
+        .filter(|x| x.contains(","))
+        .map(|y| y.split(",").map(|z| z.parse::<i64>().unwrap()).collect())
+        .collect();
+    let order: Vec<(i64, i64)> = inp
+        .iter()
+        .filter(|x| x.contains("|"))
+        .map(|y| {
+            let mut kk = y.split("|");
+            (
                 kk.next().unwrap().parse::<i64>().unwrap(),
                 kk.next().unwrap().parse::<i64>().unwrap(),
-            ));
-        }
-    });
-    (output, out)
+            )
+        })
+        .collect();
+    (order, pages)
 }
 
 pub fn check_or(inp: &Vec<(i64, i64)>, val1: i64, val2: i64) -> Ordering {
@@ -33,8 +35,7 @@ pub fn run(inp: Vec<String>) -> (i64, i64) {
     for v in pages.iter() {
         let mut copy = v.clone();
         copy.sort_by(|z, zz| check_or(&order, *z, *zz));
-        let same = !v.iter().zip(&copy).any(|(j, jj)| *j != *jj);
-        if same {
+        if !v.iter().zip(&copy).any(|(j, jj)| *j != *jj) {
             acc += copy.get(copy.len() / 2).unwrap();
         } else {
             acc2 += copy.get(copy.len() / 2).unwrap();
--- src/y2024/day06.rs
+++ src/y2024/day06.rs
@@ -1,3 +1,67 @@
+use rayon::iter::{IntoParallelIterator, ParallelIterator};
+
+pub fn is_loop(map: &Vec<Vec<i64>>, pos_: (i32, i32)) -> bool {
+    let mut visited = vec![vec![(0u32, (0i32, 0i32)); map[0].len()]; map.len()];
+    let mut dir: (i32, i32) = (-1, 0);
+    let mut pos = pos_;
+    let mut limit = 1000000;
+    loop {
+        limit -= 1;
+        if limit == 0 {
+            println!("Limit reached");
+            return true;
+        }
+        let (did, prev) = visited[pos.0 as usize][pos.1 as usize];
+        if did == 1 && prev.0 == dir.0 && prev.1 == dir.1 {
+            return true;
+        }
+        visited[pos.0 as usize][pos.1 as usize] = (1, dir);
+        let mut new = pos.clone();
+        new.0 += dir.0;
+        new.1 += dir.1;
+        if new.0 >= map.len() as i32 || new.1 >= map[0].len() as i32 || new.0 < 0 || new.1 < 0 {
+            return false;
+        } else if map[new.0 as usize][new.1 as usize] == 1 {
+            if dir.0 == -1 {
+                dir.0 = 0;
+                dir.1 = 1;
+            } else if dir.1 == 1 {
+                dir.0 = 1;
+                dir.1 = 0;
+            } else if dir.0 == 1 {
+                dir.0 = 0;
+                dir.1 = -1;
+            } else if dir.1 == -1 {
+                dir.0 = -1;
+                dir.1 = 0;
+            }
+        } else {
+            pos = new.clone();
+        }
+    }
+}
+
+pub fn part2(mapp: Vec<Vec<i64>>, pos_: (i32, i32)) -> i64 {
+    //for i in 0..map.len() {
+    (0..mapp.len())
+        .into_par_iter()
+        .map(|i| {
+            let mut map = mapp.clone();
+            let mut acc = 0;
+            for j in 0..map[0].len() {
+                if map[i][j] == 0 {
+                    map[i][j] = 1;
+                    if is_loop(&map, pos_) {
+                        acc += 1;
+                    }
+                    map[i][j] = 0;
+                }
+            }
+            acc
+        })
+        .sum()
+}
+
 pub fn part1(map: &Vec<Vec<i64>>, pos_: (i32, i32)) -> i64 {
     let mut visited = vec![vec![0u32; map[0].len()]; map.len()];
     let mut dir: (i32, i32) = (-1, 0);
@@ -7,7 +71,7 @@ pub fn part1(map: &Vec<Vec<i64>>, pos_: (i32, i32)) -> i64 {
         let mut new = pos.clone();
         new.0 += dir.0;
         new.1 += dir.1;
-        if new.0 == map.len() as i32 || new.1 == map[0].len() as i32 || new.0 < 0 || new.1 < 0 {
+        if new.0 >= map.len() as i32 || new.1 >= map[0].len() as i32 || new.0 < 0 || new.1 < 0 {
             break;
         } else if map[new.0 as usize][new.1 as usize] == 1 {
             if dir.0 == -1 {
@@ -34,7 +98,19 @@ pub fn part1(map: &Vec<Vec<i64>>, pos_: (i32, i32)) -> i64 {
 pub fn run(inp: Vec<String>) -> (i64, i64) {
     let map: Vec<Vec<i64>> = inp
         .iter()
-        .map(|x| x.chars().map(|y| if y == '#' { 1 } else { 0 }).collect())
+        .map(|x| {
+            x.chars()
+                .map(|y| {
+                    if y == '#' {
+                        1
+                    } else if y == '^' {
+                        2
+                    } else {
+                        0
+                    }
+                })
+                .collect()
+        })
         .collect();
     let mut pos: (i32, i32) = (0, 0);
     for (i, x) in inp.iter().enumerate() {
@@ -44,5 +120,5 @@ pub fn run(inp: Vec<String>) -> (i64, i64) {
             }
         }
     }
-    (part1(&map, pos), 0)
+    (part1(&map, pos), part2(map, pos))
 }
