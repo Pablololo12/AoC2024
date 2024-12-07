f43564a4909c5739b2c6619354ad163e40d0a956
Pablololo12 <pabloheralm@gmail.com>
2024-12-07 21:04:56 +0000

    Day7 optimised

--- src/y2024/day07.rs
+++ src/y2024/day07.rs
@@ -1,27 +1,44 @@
-fn search(test: i64, acc: i64, inn: &[i64]) -> bool {
+use rayon::iter::{IntoParallelIterator, ParallelIterator};
+
+fn search_p2(acc: i64, inn: &[i64]) -> bool {
     if inn.len() == 0 {
-        return test == acc;
-    } else if acc > test {
+        return acc == 0;
+    } else if acc <= 0 {
         return false;
     }
-    let sum_path = search(test, acc + inn.first().unwrap(), &inn[1..]);
-    let mul_path = search(test, acc * inn.first().unwrap(), &inn[1..]);
-    sum_path || mul_path
+
+    let head: i64 = inn[0];
+    let num_digits = head.checked_ilog10().unwrap_or(0) as u32 + 1;
+    let hundred = 10_i64.pow(num_digits);
+
+    let sum_path = search_p2(acc - head, &inn[1..]);
+    let mul_path = match acc % head {
+        0 => search_p2(acc / head, &inn[1..]),
+        _ => false,
+    };
+    let aggregate_path = match (acc % hundred) == head {
+        true => search_p2(acc / hundred, &inn[1..]),
+        _ => false,
+    };
+    sum_path || mul_path || aggregate_path
 }
 
-fn search2(test: i64, acc: i64, inn: &[i64]) -> bool {
+fn search_p1(acc: i64, inn: &[i64]) -> bool {
     if inn.len() == 0 {
-        return test == acc;
-    } else if acc > test {
+        return acc == 0;
+    } else if acc <= 0 {
         return false;
     }
-    let head: i64 = inn.first().unwrap().clone();
-    let num_digits = head.checked_ilog10().unwrap_or(0) as u32 + 1;
 
-    let sum_path = search2(test, acc + head, &inn[1..]);
-    let aggregate_path = search2(test, (acc * (10_i64.pow(num_digits))) + head, &inn[1..]);
-    let mul_path = search2(test, acc * head, &inn[1..]);
-    sum_path || mul_path || aggregate_path
+    let head: i64 = inn[0];
+
+    let sum_path = search_p1(acc - head, &inn[1..]);
+    let mul_path = match acc % head {
+        0 => search_p1(acc / head, &inn[1..]),
+        _ => false,
+    };
+
+    sum_path || mul_path
 }
 
 pub fn run(inp: Vec<String>) -> (i64, i64) {
@@ -35,12 +52,23 @@ pub fn run(inp: Vec<String>) -> (i64, i64) {
                     .unwrap()
                     .split_whitespace()
                     .map(|x| x.parse::<i64>().unwrap())
+                    .rev()
                     .collect(),
             )
         })
         .collect();
-    let p1 = inn.iter().filter(|z| search(z.0, 0, &z.1)).map(|(u, _)| u).sum();
-    let p2 = inn.iter().filter(|z| search2(z.0, 0, &z.1)).map(|(u, _)| u).sum();
+
+    let p1 = inn
+        .clone()
+        .into_par_iter()
+        .filter(|z| search_p1(z.0, &z.1))
+        .map(|(u, _)| u)
+        .sum();
+    let p2 = inn
+        .into_par_iter()
+        .filter(|z| search_p2(z.0, &z.1))
+        .map(|(u, _)| u)
+        .sum();
 
     (p1, p2)
 }
