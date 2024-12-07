c11dc983f4caaa540db195aa0acd0b4417d4805b
Pablololo12 <pabloheralm@gmail.com>
2024-12-07 11:02:22 +0000

    Day7 cleaner

--- src/y2024/day07.rs
+++ src/y2024/day07.rs
@@ -1,50 +1,27 @@
-fn search(test: i64, acc: i64, inn: &[i64]) -> i64 {
-    if inn.len() == 0 && test == acc {
-        return test;
-    } else if inn.len() == 0 {
-        return 0;
+fn search(test: i64, acc: i64, inn: &[i64]) -> bool {
+    if inn.len() == 0 {
+        return test == acc;
     } else if acc > test {
-        return 0;
+        return false;
     }
     let sum_path = search(test, acc + inn.first().unwrap(), &inn[1..]);
-    let mut mul_path = 0;
-    if acc != 0 {
-        mul_path = search(test, acc * inn.first().unwrap(), &inn[1..]);
-    }
-    if sum_path != 0 || mul_path != 0 {
-        return test;
-    }
-    0
+    let mul_path = search(test, acc * inn.first().unwrap(), &inn[1..]);
+    sum_path || mul_path
 }
 
-fn search2(test: i64, acc: i64, inn: &[i64]) -> i64 {
-    if inn.len() == 0 && test == acc {
-        return test;
-    } else if inn.len() == 0 {
-        return 0;
+fn search2(test: i64, acc: i64, inn: &[i64]) -> bool {
+    if inn.len() == 0 {
+        return test == acc;
     } else if acc > test {
-        return 0;
+        return false;
     }
     let head: i64 = inn.first().unwrap().clone();
     let num_digits = head.checked_ilog10().unwrap_or(0) as u32 + 1;
+
     let sum_path = search2(test, acc + head, &inn[1..]);
     let aggregate_path = search2(test, (acc * (10_i64.pow(num_digits))) + head, &inn[1..]);
-    let mut mul_path = 0;
-    if acc != 0 {
-        mul_path = search2(test, acc * head, &inn[1..]);
-    }
-    if sum_path != 0 || mul_path != 0 || aggregate_path != 0 {
-        return test;
-    }
-    0
-}
-
-fn part1(test: i64, inn: &Vec<i64>) -> i64 {
-    search(test, 0, &inn[..])
-}
-
-fn part2(test: i64, inn: &Vec<i64>) -> i64 {
-    search2(test, 0, &inn[..])
+    let mul_path = search2(test, acc * head, &inn[1..]);
+    sum_path || mul_path || aggregate_path
 }
 
 pub fn run(inp: Vec<String>) -> (i64, i64) {
@@ -62,8 +39,8 @@ pub fn run(inp: Vec<String>) -> (i64, i64) {
             )
         })
         .collect();
-    let p1 = inn.iter().map(|z| part1(z.0, &z.1)).sum();
-    let p2 = inn.iter().map(|z| part2(z.0, &z.1)).sum();
+    let p1 = inn.iter().filter(|z| search(z.0, 0, &z.1)).map(|(u, _)| u).sum();
+    let p2 = inn.iter().filter(|z| search2(z.0, 0, &z.1)).map(|(u, _)| u).sum();
 
     (p1, p2)
 }
