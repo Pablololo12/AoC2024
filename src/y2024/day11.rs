ca037ac3c7e53e0bb85b98c75e38904e5a3a328e
Pablololo12 <pabloheralm@gmail.com>
2024-12-11 13:47:45 +0000

    Simplified day11 to recursive

--- src/y2024/day11.rs
+++ src/y2024/day11.rs
@@ -1,47 +1,29 @@
-use std::collections::HashMap;
+use cached::proc_macro::cached;
 
-use rayon::iter::{IntoParallelIterator, ParallelIterator};
-
-fn merge_pairs(p: Vec<(u64, u64)>) -> Vec<(u64, u64)> {
-    let mut counts: HashMap<u64, u64> = HashMap::new();
-
-    p.iter().for_each(|(number, count)| {
-        *counts.entry(*number).or_insert(0) += count;
-    });
-
-    counts.into_iter().collect()
-}
-
-fn solve(mut v: Vec<(u64, u64)>, n: u64) -> u64 {
-    for _ in 0..n {
-        for i in 0..v.len() {
-            if v[i].0 == 0 {
-                v[i].0 = 1;
-            } else {
-                let num_digits = v[i].0.checked_ilog10().unwrap_or(0) as u32 + 1;
-                if num_digits % 2 == 0 {
-                    let dv = 10_u64.pow(num_digits / 2);
-                    v.push((v[i].0 % dv, v[i].1));
-                    v[i].0 = v[i].0 / dv;
-                } else {
-                    v[i].0 *= 2024;
-                }
-            }
-        }
-        v = merge_pairs(v);
+#[cached]
+fn recursive_solv(n: u64, d: u64) -> u64 {
+    if d == 0 {
+        return 1;
     }
-    v.iter().fold(0u64, |acc, w| acc + w.1 as u64)
+    let digs = n.checked_ilog10().unwrap_or(0) + 1;
+    let count = match n {
+        0 => recursive_solv(1, d - 1),
+        y if digs % 2 == 0 => {
+            recursive_solv(y / 10_u64.pow(digs / 2), d - 1) + recursive_solv(y % 10_u64.pow(digs / 2), d - 1)
+        }
+        _ => recursive_solv(n * 2024, d - 1),
+    };
+    count
 }
 
 pub fn run(inp: Vec<String>) -> (i64, i64) {
-    let arr: Vec<(u64, u64)> = inp
+    let arr: Vec<u64> = inp
         .first()
         .unwrap()
         .split_whitespace()
-        .map(|f| (f.parse::<u64>().unwrap(), 1))
+        .map(|f| f.parse::<u64>().unwrap())
         .collect();
-    let p1: u64 = arr.clone().into_par_iter().map(|(w, _)| solve(vec![(w, 1)], 25)).sum();
-    let p2: u64 = arr.into_par_iter().map(|(w, _)| solve(vec![(w, 1)], 75)).sum();
+    let p1: u64 = arr.iter().map(|w| recursive_solv(*w, 25)).sum();
+    let p2: u64 = arr.iter().map(|w| recursive_solv(*w, 75)).sum();
     (p1 as i64, p2 as i64)
-    //(solve(arr.clone(), 25) as i64, solve(arr, 75) as i64)
 }
