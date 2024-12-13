5aac0e2fd1d80a09c21f49e37ca4e400b5c89d36
Pablololo12 <pabloheralm@gmail.com>
2024-12-13 08:59:39 +0000

    Improved day 13

--- src/y2024/day13.rs
+++ src/y2024/day13.rs
@@ -1,75 +1,45 @@
+use itertools::izip;
 use regex::Regex;
 
-pub fn run(inp: Vec<String>) -> (i64, i64) {
+fn get_in(inp: &Vec<String>, mt: &str) -> Vec<(i64, i64)> {
     let re = Regex::new(r"([0-9]+)[^0-9]*([0-9]+)").unwrap();
-    let mut total = 0;
-    let mut total2 = 0;
-    let mut counter = 0;
-    let mut ax = 0;
-    let mut ay = 0;
-    let mut bx = 0;
-    let mut by = 0;
-    let mut x = 0;
-    let mut y = 0;
-    for i in inp {
-        if i.contains('A') {
-            if let Some(c) = re.captures(&i) {
-                ax = c.get(1).unwrap().as_str().parse::<i64>().unwrap();
-                ay = c.get(2).unwrap().as_str().parse::<i64>().unwrap();
-                counter += 1;
-            }
-        }
-        if i.contains("B:") {
-            if let Some(c) = re.captures(&i) {
-                bx = c.get(1).unwrap().as_str().parse::<i64>().unwrap();
-                by = c.get(2).unwrap().as_str().parse::<i64>().unwrap();
-                counter += 1;
-            }
-        }
-        if i.contains('=') {
-            if let Some(c) = re.captures(&i) {
-                x = c.get(1).unwrap().as_str().parse::<i64>().unwrap();
-                y = c.get(2).unwrap().as_str().parse::<i64>().unwrap();
-                counter += 1;
-            }
-        }
-        if counter == 3 {
-            println!("{} {} {} {}", ax, ay, bx, by);
-            counter = 0;
-            let det = ax * by - bx * ay;
-            if det == 0 {
-                println!("WHOOPS");
-                continue;
-            }
-            println!("{det}");
-            let iax = by;
-            let ibx = -bx;
-            let iay = -ay;
-            let iby = ax;
-            println!("{} {} {} {}", iax, iay, ibx, iby);
-            let mut a = iax * x + ibx * y;
-            let mut b = iay * x + iby * y;
-            a /= det;
-            b /= det;
-            let res1 = a * ax + b * bx;
-            let res2 = a * ay + b * by;
-            if res1 == x && res2 == y {
-                println!("{} {}", a, b);
-                total += a * 3 + b;
-            }
-            x += 10000000000000;
-            y += 10000000000000;
-            let mut a2 = iax * x + ibx * y;
-            let mut b2 = iay * x + iby * y;
-            a2 /= det;
-            b2 /= det;
-            let res12 = a2 * ax + b2 * bx;
-            let res22 = a2 * ay + b2 * by;
-            if res12 == x && res22 == y {
-                println!("*{} {}", a2, b2);
-                total2 += a2 * 3 + b2;
-            }
+    inp.iter()
+        .filter(|w| w.contains(&mt))
+        .map(|w| re.captures(w).unwrap())
+        .map(|w| {
+            (
+                w.get(1).unwrap().as_str().parse::<i64>().unwrap(),
+                w.get(2).unwrap().as_str().parse::<i64>().unwrap(),
+            )
+        })
+        .collect()
+}
+
+fn solver(ax: i64, ay: i64, bx: i64, by: i64, x: i64, y: i64) -> i64 {
+    let det = ax * by - bx * ay;
+    if det != 0 {
+        let iax = by;
+        let ibx = -bx;
+        let iay = -ay;
+        let iby = ax;
+        let a = (iax * x + ibx * y) / det;
+        let b = (iay * x + iby * y) / det;
+        let res1 = a * ax + b * bx;
+        let res2 = a * ay + b * by;
+        if res1 == x && res2 == y {
+            return a * 3 + b;
         }
     }
-    (total, total2)
+    0
+}
+
+pub fn run(inp: Vec<String>) -> (i64, i64) {
+    izip!(get_in(&inp, "A:"), get_in(&inp, "B:"), get_in(&inp, "="))
+        .map(|((ax, ay), (bx, by), (x, y))| {
+            (
+                solver(ax, ay, bx, by, x, y),
+                solver(ax, ay, bx, by, x + 10000000000000, y + 10000000000000),
+            )
+        })
+        .fold((0, 0), |(a, aa), (i, j)| (a + i, aa + j))
 }
