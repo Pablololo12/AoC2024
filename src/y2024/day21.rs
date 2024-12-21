c5c23d1e7584db11dffc0fddc7f97d26778f5876
Pablololo12 <pabloheralm@gmail.com>
2024-12-21 08:54:45 +0100

    Day21 de duplicated code

--- src/y2024/day21.rs
+++ src/y2024/day21.rs
@@ -5,9 +5,25 @@ use std::{
     usize::{self, MAX},
 };
 
+fn where_robot(to_enter: String, numpads: usize) -> usize {
+    if numpads == 0 {
+        return to_enter.len();
+    }
+    let mut current = 'A';
+    let mut result = 0;
+    for c in to_enter.chars() {
+        result += where_numpad(current, c, numpads, false);
+        current = c
+    }
+    result
+}
+
 #[cached]
-fn where_control(prev_pos: char, new_pos: char, numpads: usize) -> usize {
-    let num_pad = vec!['C', '^', 'A', '<', 'v', '>'];
+fn where_numpad(prev_pos: char, new_pos: char, numpads: usize, keys: bool) -> usize {
+    let num_pad = match keys {
+        true => vec!['7', '8', '9', '4', '5', '6', '1', '2', '3', 'C', '0', 'A'],
+        _ => vec!['C', '^', 'A', '<', 'v', '>'],
+    };
     let pp = num_pad.iter().position(|w| *w == prev_pos).unwrap();
     let np = num_pad.iter().position(|w| *w == new_pos).unwrap();
     let pp = (pp / 3, pp % 3);
@@ -20,51 +36,9 @@ fn where_control(prev_pos: char, new_pos: char, numpads: usize) -> usize {
             solution = min(solution, where_robot(typed + "A", numpads - 1));
             continue;
         }
-        if sc.0 == 0 && sc.1 == 0 {
+        if keys && sc.0 == 3 && sc.1 == 0 {
             continue;
-        } else {
-            if sc.0 < np.0 {
-                open.push_back(((sc.0 + 1, sc.1), typed.clone() + "v"));
-            } else if sc.0 > np.0 {
-                open.push_back(((sc.0 - 1, sc.1), typed.clone() + "^"));
-            }
-            if sc.1 < np.1 {
-                open.push_back(((sc.0, sc.1 + 1), typed + ">"));
-            } else if sc.1 > np.1 {
-                open.push_back(((sc.0, sc.1 - 1), typed + "<"));
-            }
-        }
-    }
-    solution
-}
-fn where_robot(to_enter: String, numpads: usize) -> usize {
-    if numpads == 0 {
-        return to_enter.len();
-    }
-    let mut current = 'A';
-    let mut result = 0;
-    for c in to_enter.chars() {
-        result += where_control(current, c, numpads);
-        current = c
-    }
-    result
-}
-
-fn where_numpad(prev_pos: char, new_pos: char, numpads: usize) -> usize {
-    let num_pad = vec!['7', '8', '9', '4', '5', '6', '1', '2', '3', 'C', '0', 'A'];
-    let pp = num_pad.iter().position(|w| *w == prev_pos).unwrap();
-    let np = num_pad.iter().position(|w| *w == new_pos).unwrap();
-    let pp = (pp / 3, pp % 3);
-    let np = (np / 3, np % 3);
-    let mut open: VecDeque<((usize, usize), String)> = VecDeque::new();
-    open.push_back((pp, "".to_string()));
-    let mut solution = MAX;
-    while let Some((sc, typed)) = open.pop_front() {
-        if np.0 == sc.0 && np.1 == sc.1 {
-            solution = min(solution, where_robot(typed + "A", numpads));
-            continue;
-        }
-        if sc.0 == 3 && sc.1 == 0 {
+        } else if !keys && sc.0 == 0 && sc.1 == 0 {
             continue;
         } else {
             if sc.0 < np.0 {
@@ -89,12 +63,12 @@ pub fn run(inp: Vec<String>) -> (String, String) {
         let mut current = 'A';
         let mut result = 0;
         for c in line.chars() {
-            result += where_numpad(current, c, 2);
+            result += where_numpad(current, c, 3, true);
             current = c
         }
         let mut result2 = 0;
         for c in line.chars() {
-            result2 += where_numpad(current, c, 25);
+            result2 += where_numpad(current, c, 26, true);
             current = c
         }
 
